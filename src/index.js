e36de8bcd53d45be71d9bc0addf3c8dff29439d0
Rachit Gupta <70112715+rachit615@users.noreply.github.com>
2024-02-11 21:57:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-// export const server = `https://api.coingecko.com/api/v3`;
-export const server = `https://api.coingecko.com/api/v3/coins/list?include_platform=true`;
+export const server = `https://api.coingecko.com/api/v3`;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
