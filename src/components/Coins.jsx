d05f9228b37869bb66bddd53d3d245a2bb7d06c0
Rachit Gupta <70112715+rachit615@users.noreply.github.com>
2024-02-11 21:46:07 +0530

    Update Coins.jsx

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -17,7 +17,7 @@ const Coins = () => {
   useEffect(() => {
     const fetchCoins = async () => {
       try {
-        const { data } = await axios.get(`${server}/coins/list/`);
+        const { data } = await axios.get(`${server}`);
         setCoins(data);
         // console.log(data);
         setLoading(false);
